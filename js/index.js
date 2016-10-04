85acfe1d2ed3a299eac32ce5d24e3a95d7de5972
Slava <randomnuy@gmail.com>
2016-10-04 16:46:00 +0000

    Brrr

---
+++ js/index.js
@@ -0,0 +1,15 @@
+for (var i = 2; i <= 10 ; i = i + 2 ) {
+    document.write(i + '<br>')
+}
+
+var j = 0;
+while  (j <= 10) {
+    document.write(j + '<br>');
+    j++;
+}
+
+var k = 0;
+do {
+    document.write(k + '<br>');
+    k++;
+} while (k <= 10)
